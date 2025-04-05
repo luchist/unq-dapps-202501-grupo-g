1ce99c15a15c9fe7b23fdc7421c874130f633435
Luis Coronel <lucho_9292@live.com>
2025-04-05 01:46:05 -0300

    initial commit :octocat:
    spring initializr and intellij configured

---
+++ football-stats-predictions/src/main/kotlin/com/footballdata/football_stats_predictions/FootballStatsPredictionsApplication.kt
@@ -0,0 +1,11 @@
+package com.footballdata.football_stats_predictions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FootballStatsPredictionsApplication
+
+fun main(args: Array<String>) {
+	runApplication<FootballStatsPredictionsApplication>(*args)
+}
---
+++ football-stats-predictions/src/test/kotlin/com/footballdata/football_stats_predictions/FootballStatsPredictionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.footballdata.football_stats_predictions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FootballStatsPredictionsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
