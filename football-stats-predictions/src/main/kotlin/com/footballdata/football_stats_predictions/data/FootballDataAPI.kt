913508e4c6b1de98a431cddaf3edce7893f4d324
Luis Coronel <lucho_9292@live.com>
2025-05-24 21:31:28 -0300

    deleted unused methods on FootballDataAPI

--- football-stats-predictions/src/main/kotlin/com/footballdata/football_stats_predictions/data/FootballDataAPI.kt
+++ football-stats-predictions/src/main/kotlin/com/footballdata/football_stats_predictions/data/FootballDataAPI.kt
@@ -16,31 +16,7 @@ class FootballDataAPI(
     val connectionFactory: (String) -> HttpURLConnection = { urlString: String ->
         URL(urlString).openConnection() as HttpURLConnection
     }
-) : StatisticsProvider {
-
-    override fun getTeamStatistics(teamName: String): TeamStatistics {
-        TODO("Not yet implemented")
-    }
-
-    override fun getPlayerStatistics(playerName: String): PlayerStatistics {
-        TODO("Not yet implemented")
-    }
-
-    override fun getMatchStatistics(matchId: String): MatchStatistics {
-        TODO("Not yet implemented")
-    }
-
-    fun getMatchResult(matchDate: String, leagueName: String) {
-        TODO("Not yet implemented")
-    }
-
-    fun getFixtureLeague(leagueName: String) {
-        TODO("Not yet implemented")
-    }
-
-    fun getTeamSetup(teamName: String, matchDate: String, leagueName: String) {
-        TODO("Not yet implemented")
-    }
+) {
 
     fun getTeamComposition(teamName: String): List<Player> {
 
