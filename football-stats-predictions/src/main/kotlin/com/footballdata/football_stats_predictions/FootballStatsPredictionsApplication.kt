8fa4720f9f215b1487451647307505c5af77e928
Luis Coronel <lucho_9292@live.com>
2025-06-29 20:36:29 -0300

    working logging aspect with annotation @LogFunctionCall

--- football-stats-predictions/src/main/kotlin/com/footballdata/football_stats_predictions/FootballStatsPredictionsApplication.kt
+++ football-stats-predictions/src/main/kotlin/com/footballdata/football_stats_predictions/FootballStatsPredictionsApplication.kt
@@ -1,5 +1,6 @@
 package com.footballdata.football_stats_predictions
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -8,6 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @SpringBootApplication
 class FootballStatsPredictionsApplication
 
+val logger = LoggerFactory.getLogger(FootballStatsPredictionsApplication::class.java)
+
 fun main(args: Array<String>) {
     runApplication<FootballStatsPredictionsApplication>(*args)
+
+    logger.debug("Debug log message");
+    logger.info("Info log message");
+    logger.error("Error log message");
+    logger.warn("Warn log message");
+    logger.trace("Trace log message");
 }
