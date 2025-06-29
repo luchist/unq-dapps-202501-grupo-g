f35d6812a8daab31c1c1949204a49bbea2c810d0
Luis Coronel <lucho_9292@live.com>
2025-06-29 20:43:23 -0300

    logging aspect customizable log level

--- football-stats-predictions/src/main/kotlin/com/footballdata/football_stats_predictions/aspects/LogFunctionCall.kt
+++ football-stats-predictions/src/main/kotlin/com/footballdata/football_stats_predictions/aspects/LogFunctionCall.kt
@@ -4,29 +4,44 @@ import com.footballdata.football_stats_predictions.logger
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.reflect.MethodSignature
 import org.springframework.stereotype.Component
 
 
 @Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class LogFunctionCall(val logLevel: String = "INFO")
 
 @Aspect
 @Component
-public class LogFunctionCallAspect {
+public class LogFunctionCallAspect(private val defaultLogLevel: String = "INFO") {
 
     @Around("@annotation(LogFunctionCall)")
     @Throws(Throwable::class)
     fun logExecutionTime(joinPoint: ProceedingJoinPoint): Any? {
-        // The error level can be set dynamically
+        // Get the method signature and annotation details
+        val signature = joinPoint.signature as MethodSignature
+        val method = signature.method
+        val annotation = method.getAnnotation(LogFunctionCall::class.java)
+        val logLevel = annotation?.logLevel ?: defaultLogLevel
 
         // Log the method name and arguments if any
-        if (joinPoint.args.isEmpty()) {
-            logger.info("Function called: \'${joinPoint.signature.name}\' with no arguments")
-        } else {
-            logger.info(
+        val message =
+            if (joinPoint.args.isEmpty()) {
+                "Function called: \'${joinPoint.signature.name}\' with no arguments"
+            } else {
                 "Function called: \'${joinPoint.signature.name}\'" +
                         "with arguments: ${joinPoint.args.joinToString(", ")}"
-            )
+            }
+
+        //Log level
+        when (logLevel.uppercase()) {
+            "TRACE" -> logger.trace(message)
+            "DEBUG" -> logger.debug(message)
+            "INFO" -> logger.info(message)
+            "WARN" -> logger.warn(message)
+            "ERROR" -> logger.error(message)
+            else -> logger.info(message) // default to INFO
         }
         return joinPoint.proceed()
     }
